c48cd5303b0d57c15c7ea865003a703c2be1bb9b
Sagar12100 <49357969+Sagar12100@users.noreply.github.com>
2019-04-08 22:51:30 +0530

    Add files via upload

--- roundrobin1.cpp
+++ roundrobin1.cpp
@@ -1,54 +1,36 @@
-#include<stdio.h> 
- 
-int main() 
-{ 
- 
-  int i,p,j,n,tm,remn,m=0,t_qt,x=0,s=1,tem; 
-  int wt_tm=0,turnar_tm=0,ar_tm[10],bu_tm[10],run_tm[10],prt[10],ptr[10]; 
-  printf("Enter Total number of Process:\t "); 
-  scanf("%d",&n); 
-  remn=n; 
-  for(p=0;p<n;p++) 
-  { 
-    printf("\n\n");
-    printf("Enter Arrival Time for process %d:-",p+1); 
-    scanf("%d",&ar_tm[p]); 
-    printf("Enter the Burst time for process %d:-",p+1);
-    scanf("%d",&bu_tm[p]);
-    printf("Enter the Priority for process %d:-",p+1,"\n");
-	scanf("%d",&prt[p]);
-	printf("\n");
-    run_tm[p]=bu_tm[p]; 
-  } 
-  printf("Enter Time Quantum:\t"); 
-  scanf("%d",&t_qt); 
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
-  for(tm=0,p=0;remn!=0;) 
-  { 
-    if(run_tm[p]<=t_qt && run_tm[p]>0) 
-    { 
-      tm+=run_tm[p]; 
-      run_tm[p]=0; 
-      m=1; 
-    } 
-    else if(run_tm[p]>0) 
-    { 
-      run_tm[p]-=t_qt; 
-      tm+=t_qt; 
-    } 
-    if(run_tm[p]==0 && m==1) 
-    { 
-      remn--; 
-      printf("P[%d]\t|\t%d\t|\t%d\n",p+1,tm-ar_tm[p],tm-ar_tm[p]-bu_tm[p]); 
-      wt_tm+=tm-ar_tm[p]-bu_tm[p]; 
-      turnar_tm+=tm-ar_tm[p]; 
-      m=0; 
-    } 
-    if(p==n-1) 
-      p=0; 
-    else if(ar_tm[p+1]<=tm) 
-      p++; 
-    else 
-      p=0; 
-  } 
-
+ printf("\nAverage Waiting Time= %f\n",wt_tm*1.0/n); 
+  printf("Avg Turnaround Time = %f",turnar_tm*1.0/n);
+  for(i=0;i<n;i++)
+  {
+  	ptr[i]=prt[i];
+  }
+  for (i=0;i <n;i++)
+    {
+        for (j =0; j<(n-i-1);j++)
+        {
+            if (ptr[j] >ptr[j + 1])
+            {
+                tem =ptr[j];
+                ptr[j] = ptr[j + 1];
+                ptr[j + 1] = tem;
+            }
+        }
+    }
+  for(i=0;i<n;i++)
+  {
+  	for(j=0;j<n;j++)
+  	{
+  		if((ptr[i]==prt[j])&&(ptr[i]!=0))
+  		{
+		x=j+1;
+  		printf("\nprocess %d come %d",x,s);
+  		s=s+1;
+	  }
+	  if(ptr[i]==0)
+	  {
+	  	printf("\nprocess %d come %d",s);
+	  }
+}
+  }
+  return 0; 
+}
